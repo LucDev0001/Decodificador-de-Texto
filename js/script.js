bc247df44f61945cc5346f3ada42b2d10951be71
LucDev0001 <156922180+LucDev0001@users.noreply.github.com>
2024-02-05 17:01:40 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,86 @@
+
+//PROCESSAMENTO DO TEXTO
+function processText(operation) {
+  var inputText = document.getElementById("text").value.toLowerCase();
+
+  // Verifica se o texto contém letras maiúsculas ou acentos
+  if (/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(inputText)) {
+    alert("Por favor, insira apenas letras minúsculas e sem acentos.");
+    return;
+  }
+
+  var resultTitle = document.getElementById("resultTitle");
+  var resultText = document.getElementById("resultText");
+  var imageElement = document.querySelector(".container__resultado img");
+
+  if (operation === "encrypt") {
+    showOverlay(); // Mostra a sobreposição
+    setTimeout(function () {
+      hideOverlay(); // Esconde a sobreposição após 2 segundos
+      imageElement.style.display = "none";
+      var encryptedText = encryptText(inputText);
+      resultTitle.style.display = "none";
+      // resultTitle.textContent = "Texto Criptografado";
+      resultText.textContent = encryptedText;
+    }, 2000);
+  } else if (operation === "decrypt") {
+    showOverlay(); // Mostra a sobreposição
+    setTimeout(function () {
+      hideOverlay(); // Esconde a sobreposição após 2 segundos
+      imageElement.style.display = "none"; // Esconda a imagem para descriptografia
+      var decryptedText = decryptText(inputText);
+      resultTitle.style.display = "none";
+      // resultTitle.textContent = "Texto Descriptografado";
+      resultText.textContent = decryptedText;
+    }, 2000);
+  }
+}
+
+function showOverlay() {
+  var overlay = document.createElement("div");
+  overlay.className = "overlay";
+  overlay.innerHTML =
+    '<img src="img/isometric-server-transferring-data.gif" alt="Transferindo Dados">';
+  document.body.appendChild(overlay);
+}
+
+function hideOverlay() {
+  var overlay = document.querySelector(".overlay");
+  if (overlay) {
+    document.body.removeChild(overlay);
+  }
+}
+
+//CRIPTOGRAFIA DO TEXTO
+
+function encryptText(text) {
+  return text
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
+}
+
+//DESCRIPTOGRAFIA DO TEXTO
+
+function decryptText(text) {
+  return text
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+}
+
+//BOTÃO COPIAR
+function copyToClipboard() {
+  var resultText = document.getElementById("resultText");
+  var textArea = document.createElement("textarea");
+  textArea.value = resultText.textContent;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
+  alert("Texto Copiado para a área de Transferência!");
+}
